f95b54afdb36cc45abf4cac6e23eb5e46898bcc0
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2022-11-01 20:02:31 +0100

    Add files via upload

---
+++ Escape/Classes/Player.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escape.Classes
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Life { get; set; } = 100;
+
+        public List<Item> BagOfItems = new List<Item>();
+
+        public int DecreaseLife(int damage)
+        {
+            Life -= damage;
+
+            if (Life < 1)
+            {
+                Story story = new Story();
+                story.GameOver();
+            }
+            return Life;
+        }
+
+        public void Look()
+        {
+            Console.WriteLine($"Name: {Name.ToUpper()}");
+            Console.WriteLine($"Life: {Life}%");
+        }
+
+        public void InspectBackPack()
+        {
+            Console.Clear();
+            foreach (Item i in BagOfItems)
+            {
+                Console.WriteLine($"{i.Name}, Amount: {i.Amount}");
+            }
+
+        }
+
+        public void UpdateBagWithItem(string item)
+        {
+            if (item != null)
+            {
+                Item newItem = Repository.GetAllItems().Where(x => x.Name.ToLower() == item.ToLower()).SingleOrDefault();
+                BagOfItems.Add(newItem);
+                Console.WriteLine($"Added to backpack: {newItem.Name}");
+            }
+
+        }
+        public void RemoveItemFromBag(string item)
+        {
+            if (item != null)
+            {
+                var newItem = BagOfItems
+                       .SingleOrDefault(x => x.Name.ToLower().Contains(item.ToLower()));
+                BagOfItems.Remove(newItem);
+                Console.WriteLine($"Removed from backpack: {newItem.Name}");
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ Escape/Classes/Repository.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+using System.Net.Http.Headers;
+
+namespace Escape.Classes
+{
+    public static class Repository
+    {
+        public static List<Item> GetAllItems()
+        {
+            List<Item> items = new List<Item>();
+            items.Add(new Item("match of fire", 1));
+            items.Add(new Item("dry wooden stick", 1));
+            items.Add(new Item("torch of light", 1));
+            items.Add(new Item("the ancient key", 1));
+            items.Add(new Item("the fire key", 1));
+            items.Add(new Item("the dice of fury", 1));
+            items.Add(new Item("the dice of death", 1));
+            return items;
+        }
+       
+    
+        public static List <Room> GetAllRooms()
+        {
+            Item key = GetAllItems().Where(x => x.Name.ToLower() == "the ancient key").SingleOrDefault();
+            List <Item> key01 = new List<Item> { key };
+
+            Item dice01 = GetAllItems().Where(x => x.Name.ToLower() == "the dice of fury").SingleOrDefault();
+            Item dice02 = GetAllItems().Where(x => x.Name.ToLower() == "the dice of death").SingleOrDefault();
+            List <Item> dice = new List<Item> { dice01, dice02 };
+
+            List<Room> rooms = new List<Room>();
+            rooms.Add (new Room("Room of hell, (1/2)", false, 1, 1, key01));
+            rooms.Add (new Room("Room of TEARS, (2/2)", true, 1, 1, dice));
+            return rooms;
+        }
+
+       
+    }
+}
